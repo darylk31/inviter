9838d3bb7bce9075dd144d78085b87bdb409d8a8
jtran5 <tranjimmyy@gmail.com>
2016-10-13 13:58:12 -0700

    lol

--- app/src/main/java/invite/hfad/com/inviter/MakeEventActivity.java
+++ app/src/main/java/invite/hfad/com/inviter/MakeEventActivity.java
@@ -1,16 +1,80 @@
 package invite.hfad.com.inviter;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MakeEventActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_event);
+
+        final EditText etDate = (EditText)findViewById(R.id.etDate);
+        final EditText etTime = (EditText)findViewById(R.id.etTime);
+
+        etDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Calendar mcurrentDate = Calendar.getInstance();
+                int mYear = mcurrentDate.get(Calendar.YEAR);
+                int mMonth = mcurrentDate.get(Calendar.MONTH);
+                int mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog mDatePicker;
+
+                mDatePicker = new DatePickerDialog(MakeEventActivity.this,R.style.MyDatePickerDialogTheme, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        etDate.setText(year + " " + monthOfYear + " " + dayOfMonth);
+                    }
+                }, mYear,mMonth,mDay);
+                mDatePicker.setTitle("Date");
+                mDatePicker.show();
+            }
+
+        });
+
+        etTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int mHour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int mMinute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(MakeEventActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int minute) {
+                        etTime.setText(hour + " : " + minute);
+                    }
+                }, mHour, mMinute, true);
+                mTimePicker.setTitle("Time");
+                mTimePicker.show();
+                }
+            });
     }
 
     //make discard warning for back button
     public void onInvite(){}
+
+
+
+    public void onTimeClick(){
+
+    }
 }
