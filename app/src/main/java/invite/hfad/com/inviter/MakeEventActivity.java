dc007d9c1889c610213487a5414984a2790f0f36
jtran5 <tranjimmyy@gmail.com>
2016-10-23 23:44:39 -0700

    EventMaker update

--- app/src/main/java/invite/hfad/com/inviter/MakeEventActivity.java
+++ app/src/main/java/invite/hfad/com/inviter/MakeEventActivity.java
@@ -3,6 +3,7 @@ package invite.hfad.com.inviter;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,13 @@ import java.util.Locale;
 public class MakeEventActivity extends Activity {
 
 
+    private String yearData;
+    private String monthData;
+    private String dayData;
+    private String titleData;
+    private String descriptionData;
+    private String hourData;
+    private String minuteData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +50,9 @@ public class MakeEventActivity extends Activity {
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                         etDate.setText(year + " " + monthOfYear + " " + dayOfMonth);
+                        yearData = Integer.toString(year);
+                        monthData = Integer.toString(monthOfYear);
+                        dayData = Integer.toString(dayOfMonth);
                     }
                 }, mYear,mMonth,mDay);
                 mDatePicker.setTitle("Date");
@@ -61,6 +72,8 @@ public class MakeEventActivity extends Activity {
                     @Override
                     public void onTimeSet(TimePicker view, int hour, int minute) {
                         etTime.setText(hour + " : " + minute);
+                        hourData = Integer.toString(hour);
+                        minuteData = Integer.toString(minute);
                     }
                 }, mHour, mMinute, true);
                 mTimePicker.setTitle("Time");
@@ -70,7 +83,24 @@ public class MakeEventActivity extends Activity {
     }
 
     //make discard warning for back button
-    public void onInvite(){}
+    public void onInvite(){
+        final EditText etTitle = (EditText)findViewById(R.id.etTitle);
+        final EditText etDescription = (EditText)findViewById(R.id.etDescription);
+        titleData = etTitle.getText().toString();
+        descriptionData = etDescription.getText().toString();
+
+        Intent i = new Intent(this,Contacts.class);
+        i.putExtra("<yearData>", yearData);
+        i.putExtra("<monthData>", monthData);
+        i.putExtra("<dayData>",dayData);
+        i.putExtra("hourData",hourData);
+        i.putExtra("<minuteData>",minuteData);
+        i.putExtra("<titleData>",titleData);
+        i.putExtra("<descriptionData>",descriptionData);
+
+        startActivity(i);
+
+    }
 
 
 
