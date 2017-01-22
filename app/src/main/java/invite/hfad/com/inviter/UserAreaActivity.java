f87ff6396a0120ebedc193cb7d9b3f25a5d1cefe
Shimychu <phenizer@hotmail.com>
2017-01-21 20:43:18 -0800

    Nav Drawer Click Listener

--- app/src/main/java/invite/hfad/com/inviter/UserAreaActivity.java
+++ app/src/main/java/invite/hfad/com/inviter/UserAreaActivity.java
@@ -13,12 +13,18 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -31,6 +37,8 @@ public class UserAreaActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private DrawerLayout drawerLayout;
+    public static final int GET_FROM_GALLERY = 3;
     //private FirebaseAuth auth;
     //private FirebaseAuth.AuthStateListener authListener;
 
@@ -41,7 +49,46 @@ public class UserAreaActivity extends AppCompatActivity {
         setViewPager();
         setNavigationDisplayPicture();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.drawer_nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if(item.isChecked()) item.setChecked(false);
+                else item.setChecked(true);
+
+                //Closing Drawer on item Click
+                drawerLayout.closeDrawers();
+
+                //DO when menu item is clicked
+                Intent intent;
+                switch(item.getItemId()){
+                    case R.id.nav_inbox:
+                        intent = new Intent(UserAreaActivity.this, InboxActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.nav_contacts:
+                        intent = new Intent(UserAreaActivity.this,ContactsActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.nav_settings:
+                        intent = new Intent(UserAreaActivity.this,SettingActivity.class);
+                        startActivity(intent);
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
  }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_drawer, menu);
+        return true;
+    }
 
 
         /*
@@ -62,11 +109,8 @@ public class UserAreaActivity extends AppCompatActivity {
                     startActivity(new Intent(UserAreaActivity.this, LoginActivity.class));
                 }
             }
-
             ;
-
         };
-
     }
 
     @Override
@@ -148,4 +192,23 @@ public class UserAreaActivity extends AppCompatActivity {
         Picasso.with(this).load("https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-9/14457341_10153958094445963_3322825613904558278_n.jpg?oh=49fc47777f5e5941849f092bdb12f66b&oe=58E1296D").into(profilePictureView);
 
     }
+
+    public void uploadProfilePicture(View view  ){
+        startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int id = item.getItemId();
+
+        if(id == R.id.settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
 }
