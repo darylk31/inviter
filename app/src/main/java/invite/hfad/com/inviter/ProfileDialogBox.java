abb4f93f994980fbe69e05fc22c02908f55be844
Shimychu <phenizer@hotmail.com>
2017-06-03 22:08:58 -0700

    Part 3 In CustomProfileDialog

--- app/src/main/java/invite/hfad/com/inviter/ProfileDialogBox.java
+++ app/src/main/java/invite/hfad/com/inviter/ProfileDialogBox.java
@@ -5,16 +5,20 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Jimmy on 6/3/2017.
  */
 
 public class ProfileDialogBox extends Dialog implements android.view.View.OnClickListener{
+    private Usernames user;
 
-    public ProfileDialogBox(Activity a){
+    public ProfileDialogBox(Activity a,Usernames user){
         super(a);
-
+        this.user = user;
     }
 
     @Override
@@ -22,6 +26,10 @@ public class ProfileDialogBox extends Dialog implements android.view.View.OnClic
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.custom_profile_dialog);
+        TextView profile_username = (TextView) findViewById(R.id.profile_username);
+        TextView profile_displayname = (TextView) findViewById(R.id.profile_displayname);
+        profile_username.setText(user.getUsername());
+        profile_displayname.setText(user.getDisplayname());
     }
 
     @Override
