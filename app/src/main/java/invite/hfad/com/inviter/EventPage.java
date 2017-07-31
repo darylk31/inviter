6445f44e1447fe0b4667bfaea79705081add83bc
Daryl <darylkwok333@gmail.com>
2017-07-30 20:47:45 -0700

    Uhhhh update

--- app/src/main/java/invite/hfad/com/inviter/EventPage.java
+++ app/src/main/java/invite/hfad/com/inviter/EventPage.java
@@ -52,11 +52,8 @@ import invite.hfad.com.inviter.Contacts.ContactsActivity;
 
 
 public class EventPage extends Activity {
-    private static boolean TOOLBAR_COLLAPSED = true;
     String id;
-    String event_string;
     Toolbar toolbar;
-    AppBarLayout appBarLayout;
 
     //Chat
     private static final String ANONYMOUS = "Anonymous";
@@ -83,81 +80,7 @@ public class EventPage extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_page);
         this.id = getIntent().getStringExtra("event_id");
-        try {
-            SQLiteOpenHelper eventDatabaseHelper = new UserDatabaseHelper(this.getApplicationContext());
-            SQLiteDatabase event_db = eventDatabaseHelper.getReadableDatabase();
-            Cursor cursor = event_db.rawQuery("SELECT * FROM EVENTS WHERE EID LIKE '" + id + "';", null);
-            cursor.moveToLast();
-            TextView event_name = (TextView) findViewById(R.id.tv_eventpagename);
-            this.event_string = cursor.getString(4);
-            event_name.setText(event_string);
-            TextView event_date = (TextView) findViewById(R.id.tv_eventpagedate);
-            String event_day = cursor.getString(2);
-            //TODO: Select just date.
-            try {
-                Date date = new SimpleDateFormat("yyyy-MM-dd").parse(event_day);
-                String output_day = new SimpleDateFormat("dd", Locale.ENGLISH).format(date);
-                String output_month = new SimpleDateFormat("MMM", Locale.ENGLISH).format(date);
-                String output_year = new SimpleDateFormat("yyyy", Locale.ENGLISH).format(date);
-                event_date.setText(output_month + " " + output_day + ", " + output_year);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            //TODO: Select just time.
-            TextView event_time = (TextView) findViewById(R.id.tv_eventpagetime);
-            event_time.setText(cursor.getString(2));
-            cursor.close();
-            event_db.close();
-
-        } catch (SQLiteException e) {
-            e.printStackTrace();
-            Toast toast = Toast.makeText(this.getApplicationContext(), "Error: Event unavailable", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-        this.toolbar = (Toolbar) findViewById(R.id.event_toolbar);
-        toolbar.inflateMenu(R.menu.menu_eventpage);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.invite_eventpage:
-                        Intent invite_intent = new Intent(EventPage.this, ContactsActivity.class);
-                        startActivity(invite_intent);
-                        break;
-                    case R.id.edit_eventpage:
-                        Intent edit_intent = new Intent(EventPage.this, MakeEventActivity.class);
-                        edit_intent.putExtra("Edit Id", id);
-                        startActivity(edit_intent);
-                        break;
-                    case R.id.delete_eventpage:
-                        //Alert dialog to confirm
-                        deleteEvent();
-                        Intent delete_intent = new Intent(EventPage.this, UserAreaActivity.class);
-                        startActivity(delete_intent);
-                        break;
-                }
-                return true;
-            }
-        });
-        appBarListener();
         populateChat();
-    }
-
-
-    public void deleteEvent() {
-        try {
-            SQLiteOpenHelper eventDatabaseHelper = new UserDatabaseHelper(this.getApplicationContext());
-            SQLiteDatabase event_db = eventDatabaseHelper.getWritableDatabase();
-            UserDatabaseHelper.delete_event(event_db, id);
-            event_db.close();
-            startActivity(new Intent(this, UserAreaActivity.class));
-        } catch (SQLiteException e) {
-            e.printStackTrace();
-            Toast toast = Toast.makeText(this.getApplicationContext(), "Error: Unable to delete", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-        Toast toast = Toast.makeText(this.getApplicationContext(), "Event Deleted", Toast.LENGTH_SHORT);
-        toast.show();
     }
 
     public void onSendMessage(View view) {
@@ -340,53 +263,5 @@ public class EventPage extends Activity {
             }
         });
     }
-
-        private void appBarListener(){
-            appBarLayout = (AppBarLayout) findViewById(R.id.appbar_layout);
-            appBarLayout.setExpanded(false);
-            final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-            //Get Title Showing
-            appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-                @Override
-                public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                    int collapsed_num = appBarLayout.getTotalScrollRange();
-
-                    if(verticalOffset == -collapsed_num){
-                        collapsingToolbarLayout.setTitleEnabled(true);
-                        collapsingToolbarLayout.setTitle(event_string);
-                    } else{
-                        collapsingToolbarLayout.setTitleEnabled(false);
-                    }
-                    if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()) {
-                        // Collapsed
-                        TOOLBAR_COLLAPSED = true;
-                    }
-                    else if (verticalOffset == 0) {
-                        TOOLBAR_COLLAPSED = false;
-                    } else{
-                        TOOLBAR_COLLAPSED = false;
-                    }
-                }}
-            );
-        }
-
-        @Override
-        public void onBackPressed() {
-            if(TOOLBAR_COLLAPSED){
-                super.onBackPressed();
-            } else {
-                AppBarLayout appBar = (AppBarLayout) findViewById(R.id.appbar_layout);
-                appBar.setExpanded(false, true);
-            }
-        }
-
-
-
-    /*
-    public void Test(){
-        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar_layout);
-        appBarLayout.setExpanded(false);
-    }
-    */
 
 }
