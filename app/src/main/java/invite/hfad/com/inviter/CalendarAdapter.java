ed3c4eddc28112777d8a265caf21dfb185121054
Daryl <darylkwok333@gmail.com>
2017-08-01 23:44:09 -0700

    Calendar Update

---
+++ app/src/main/java/invite/hfad/com/inviter/CalendarAdapter.java
@@ -0,0 +1,86 @@
+package invite.hfad.com.inviter;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Daryl on 8/1/2017.
+ */
+
+public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.ViewHolder> {
+
+    private String[] event_names;
+    private String[] event_times;
+    private String[] event_ids;
+    private Cursor cursor;
+
+    public CalendarAdapter(Cursor cursor){
+        this.cursor = cursor;
+        storeEvents();
+
+    }
+
+    @Override
+    public CalendarAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.calendar_list_item, parent, false);
+        return new ViewHolder(cv);
+    }
+
+    @Override
+    public void onBindViewHolder(CalendarAdapter.ViewHolder holder, final int position) {
+        final CardView cardView = holder.cardView;
+        TextView event_name = (TextView)cardView.findViewById(R.id.calendar_nameTV);
+        TextView event_time = (TextView)cardView.findViewById(R.id.calendar_timeTV);
+        event_name.setText(event_names[position]);
+        //TODO: get time.
+        //event_time.setText(event_times[position]);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), EventViewPager.class);
+                intent.putExtra("event_id", event_ids[position]);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (cursor == null){
+            return 0;
+        }
+        return cursor.getCount();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private CardView cardView;
+
+        public ViewHolder(CardView v) {
+            super(v);
+            cardView = v;
+        }
+    }
+
+    public void storeEvents() {
+        String[] names = new String[getItemCount()];
+        String[] times = new String[getItemCount()];
+        String[] ids = new String[getItemCount()];
+        for (int i = 0; i < getItemCount(); i++) {
+            cursor.moveToPosition(i);
+            names[i] = cursor.getString(4);
+            times[i] = cursor.getString(2);
+            ids[i] = cursor.getString(0);
+        }
+        this.event_names = names;
+        this.event_times = times;
+        this.event_ids = ids;
+    }
+}
--- app/src/main/java/invite/hfad/com/inviter/CalendarFragment.java
+++ app/src/main/java/invite/hfad/com/inviter/CalendarFragment.java
@@ -10,18 +10,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewDebug;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 
 public class CalendarFragment extends Fragment {
@@ -42,6 +37,15 @@ public class CalendarFragment extends Fragment {
         view = getView();
         calendarView = (MaterialCalendarView)view.findViewById(R.id.calendarView);
         recyclerView = (RecyclerView)view.findViewById(R.id.calender_recycler);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                recyclerView.setAdapter(null);
+            }
+        });
+
         calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
@@ -63,9 +67,10 @@ public class CalendarFragment extends Fragment {
                     SQLiteDatabase event_db = eventDatabaseHelper.getReadableDatabase();
                     Cursor cursor = event_db.rawQuery("SELECT * FROM EVENTS WHERE DAY LIKE '%" + converted_date + "%';", null);
                     if (cursor != null && cursor.moveToFirst()){
-                            updaterecycler(cursor);
-                            cursor.close();
+                        recyclerView.setAdapter(new CalendarAdapter(cursor));
+                        cursor.close();
                         }
+                    else {recyclerView.setAdapter(null);}
                     }
                     catch (Exception e){
                         e.printStackTrace();
@@ -75,23 +80,6 @@ public class CalendarFragment extends Fragment {
                     }
             }
         });
-
-    }
-
-    private void updaterecycler(Cursor cursor){
-        int num = cursor.getCount();
-        event_names = new String[num];
-        event_ids = new String[num];
-        for (int i = 0; i < num; i++) {
-            cursor.moveToPosition(i);
-            event_names[i] = cursor.getString(4);
-            event_ids[i] = cursor.getString(0);
-        }
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
-
-        //TODO: make adapter
-
 
     }
 
