22ef5830f4f9ad57c80ef082b455e5fcf612106d
Shimychu <phenizer@hotmail.com>
2017-10-27 21:25:00 -0700

    MyApplication class

---
+++ app/src/main/java/invite/hfad/com/MyApplication.java
@@ -0,0 +1,11 @@
+package invite.hfad.com;
+
+import android.app.Application;
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Jimmy on 10/27/2017.
+ */
+
+public class MyApplication extends MultiDexApplication {
+}
